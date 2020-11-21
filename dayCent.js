3acfb74e4bf3d50fb061235ccfe987542e2f21d2
Austin Arrington <austinarrington@Austins-MacBook-Pro.local>
2020-11-20 16:48:40 -0800

    added N2O model v1

--- dayCent.js
+++ dayCent.js
@@ -14,6 +14,13 @@ function returnMax(inputList) {
     return maxVal;
 };
 
+function returnN2O(airMin, airMax, rainMin, rainMax, claySample, NO3, NH4) {
+   
+    var N2O = airMin + airMax + rainMin + rainMax + claySample[0] + NO3[0] + NH4[0];
+    
+    return N2O; 
+}; 
+
 // INPUT 1: AIR TEMPERATURE
 // sample last 24 hrs airTemp from sensor head readings
 let airTempSample = [20,21,23,20,21,23,20,21,23,20,21,23,20,21,23,20,21,23,20,21,23,20,21,23]
@@ -31,4 +38,22 @@ console.log("Precipitation Prob Min (%): ", rainMin);
 console.log("Airtemp Max (%): ", rainMax); 
 
 // INPUT 3: SoilTextureClass
+// percent Sand / Silt / Clay 
+// SAND - 6in, 18in, 36in 
+let sandSample = [10, 10, 8]; 
+let siltSample = [64, 64, 8];
+let claySample = [26, 26, 24]; 
+console.log("6in Sand (%): ", sandSample[0]);
+console.log("18in Sand (%): ", sandSample[1]);
+console.log("36in Sand (%): ", sandSample[2]);
 
+// Vegetation type, cultivating, planting scheudle, timing of nutrient amenndments .... maybe use NDVI as input? 
+
+// Nitrate 6in, 18in, 36in  PPM 
+let NO3 = [22, 23, 30]
+// Ammonium 6in, 18in, 36in  PPM 
+let NH4 = [10, 12, 10]
+
+// OUTPUT 
+// N20 
+console.log("N2O: ", returnN2O(airMin, airMax, rainMin, rainMax, claySample, NO3, NH4));
