2bcfcbeb27a2fafee76b2ffb17549ccb4c79c9de
Austin Arrington <austinarrington@Austins-MacBook-Pro.local>
2021-04-14 15:11:29 -0400

    added pH and EC

--- teraScore.js
+++ teraScore.js
@@ -2,9 +2,13 @@
 
 // initialize weights
 var teraW1;
+var teraW2;
+var teraW3;
 
 // import values and assign weights
 var AWC = 20
+var pH = 6.2
+var ec = 5.0;
 //console.log(AWC);
 
  if (AWC==null) {
@@ -24,4 +28,50 @@ var AWC = 20
         teraW1 = 0;
     }
     
-console.log(teraW1);
+console.log("Weight 1: ", teraW1);
+
+if (pH==null) {
+    teraW2 = null;    
+} else if (pH >= 0 && pH < 3.00) {
+    teraW2 = 3.125;
+} else if (pH >= 3.00 && pH < 5.00) {
+    teraW2 = 6.250;
+} else if (pH >= 5.00 && pH < 6.00) {
+    teraW2 = 9.375;
+} else if (pH >= 6.00 && pH < 7.00) {
+    teraW2 = 12.499;
+} else if (pH >= 7.00 && pH < 7.60) {
+    teraW2 = 9.375;
+} else if (pH >= 7.60 && pH < 8.10) {
+    teraW2 = 6.250;
+} else if (pH >= 8.10 && pH < 11.00) {
+    teraW2 = 3.125;
+} else {
+    teraW2 = 0
+}
+
+console.log("Weight 2: ", teraW2);
+
+if (ec==null) {
+    teraW3 = null;
+} else if (ec==0) {
+    teraW3 = 3.125;
+} else if (ec > 0 && ec < 0.10) {
+    teraW3 = 6.250;
+} else if (ec >= 0.10 && ec < 0.25) {
+    teraW3 = 9.375;
+} else if (ec >= 0.25 && ec < 2.00) {
+    teraW3 = 12.499;
+} else if (ec >= 2.00 && ec < 4.00) {
+    teraW3 = 12.499;
+} else if (ec >= 4.00 && ec < 8.00) {
+    teraW3 = 9.375;
+} else if (ec >= 8.00 && ec < 16.00) {
+    teraW3 = 6.250;
+} else if (ec >= 16.00 && ec <= 17.00) {
+    teraW3 = 3.125;
+} else {
+    teraW3 = 0;
+}
+
+console.log("Weight 3: ", teraW3);
