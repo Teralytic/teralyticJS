814c9a7a2518ba443cb3948d3501b3e79afb20be
Austin Arrington <austinarrington@Austins-MacBook-Pro.local>
2021-04-14 13:40:47 -0400

    added awc to terascore

--- teraScore.js
+++ teraScore.js
@@ -1,6 +1,27 @@
 'use strict';
 
-// import values 
-var AWC = 21
-console.log(AWC);
+// initialize weights
+var teraW1;
 
+// import values and assign weights
+var AWC = 20
+//console.log(AWC);
+
+ if (AWC==null) {
+        teraW1 = null;
+    } else if (AWC >= 0 && AWC < 15) {
+        teraW1 = 1.563;
+    } else if (AWC >= 15 && AWC < 25) {
+        teraW1 = 3.125;
+    }
+      else if (AWC >= 25 && AWC < 50) {
+        teraW1 = 6.250;
+    } else if (AWC >= 50 && AWC < 75) {
+        teraW1 = 9.375;
+    } else if (AWC >= 75 && AWC <= 100) {
+        teraW1 = 12.499
+    } else {
+        teraW1 = 0;
+    }
+    
+console.log(teraW1);
